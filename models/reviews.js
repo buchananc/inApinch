e1b7db4a5af7f40b235c44274bf7e26876e0b62e
Tyler Staples <TylerJStaples@gmail.com>
2018-06-30 13:24:55 -0400

    Added Username Column

--- models/reviews.js
+++ models/reviews.js
@@ -1,5 +1,9 @@
 module.exports = function(sequelize, DataTypes){
     var Reviews = sequelize.define("Reviews", {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
         remarks: {
             type: DataTypes.TEXT,
             allowNull: true
